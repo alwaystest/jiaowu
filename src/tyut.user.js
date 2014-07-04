e1abe8b4dc9094df4075906f9dc1150231e8ae71
alwaystest <ericceshi1@163.com>
2014-07-04 11:22:08 +0800

    ff support only

--- src/tyut.user.js
+++ src/tyut.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name  crack tyut
-// @description 破解教务评教时间
+// @description 破解教务评教时间 firefox支持only chrome请安装https://github.com/alwaystest/jiaowu/raw/00ff4aae3574cc2affad40ca77284558c6573a4f/src/tyut.user.js
 // @author	eric
 // @version 1.2.2
 // @include *jwcurp.tyut.edu.cn:80*
@@ -20,19 +20,21 @@ var i=0;
 var timeout=1000;
 console.log("start");
 noalert();
-document.onreadystatechange = function () {
-	noalert();
-    if (document.readyState == "complete") {
-        console.log("complete");
-		pg();
-		if (unsafeWindow.flag==false){
-			setTimeout(submit,timeout);
-		}
-    }
-}
+  document.onreadystatechange = function () {
+  noalert();
+  if (document.readyState == "complete") {
+  console.log("complete");
+  pg();
+  if (unsafeWindow.flag==false){
+  setTimeout(submit,timeout);
+  }
+  }
+  }
+
+
 function pg(){
-var pic=document.getElementsByTagName('img');
-if(pic[14]){
+	var pic=document.getElementsByTagName('img');
+	if(pic[14]){
 		console.log("choose");
 		for(i=0;i<pic.length;i++){
 			if(pic[i].title=="评估")
@@ -45,44 +47,44 @@ if(pic[14]){
 		else{
 			console.log("评完啦啦啦！！！");
 		}
-	//待完成,获取成功,等待跳转页面后的处理
-}
+		//待完成,获取成功,等待跳转页面后的处理
+	}
 }
 function submit(){
-if (unsafeWindow.flag==false){
-	console.log("flag");
-	console.log(unsafeWindow.flag);
-	var aim = document.getElementsByName('zgpj');
-	var rando=rnd(start,end);
-	aim[0].value=arr[rando];
-	var check=document.getElementsByTagName('input');
-	for(i=0;i<check.length;i++){
-		if(check[i].value=='10_1'){
-			check[i].checked=true;
+	if (unsafeWindow.flag==false){
+		console.log("flag");
+		console.log(unsafeWindow.flag);
+		var aim = document.getElementsByName('zgpj');
+		var rando=rnd(start,end);
+		aim[0].value=arr[rando];
+		var check=document.getElementsByTagName('input');
+		for(i=0;i<check.length;i++){
+			if(check[i].value=='10_1'){
+				check[i].checked=true;
+			}
 		}
+		unsafeWindow.setFlag();
+		unsafeWindow.hideDiv();
+		unsafeWindow.check();//自动提交
+		//unsafeWindow.document.StDaForm.submit();//自动提交
 	}
-	unsafeWindow.setFlag();
-	unsafeWindow.hideDiv();
-	unsafeWindow.check();//自动提交
-	//unsafeWindow.document.StDaForm.submit();//自动提交
-}
 }
 function next(){
 	if(unsafeWindow.document.title=="问卷评估结果"){
 		unsafeWindow.location.href='jxpgXsAction.do?oper=listWj';
 	}
 }
-	function noalert(){
-		var _alert = alert;
-		unsafeWindow.alert = function(s) {
+function noalert(){
+	var _alert = alert;
+	unsafeWindow.alert = function(s) {
 		if(s=="评估失败，请返回！")
 			unsafeWindow.alert("评的快了，请歇息一会");
-        console.log(s);
-}
+		console.log(s);
 	}
+}
 function stop(){
 	window.stop();
-	}	
+}	
 function rnd(start, end){
 	return Math.floor(Math.random() * (end - start) + start);
 }
